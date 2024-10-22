c3845168bf370dd4e17cd0c3d68a59410ca9438c
thahit7 <147615676+thahit7@users.noreply.github.com>
2024-10-22 17:37:29 +0700

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash/
 echo "install component"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common \
 # add key GPG
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
 # add docker repository to source apt
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" \
 # Update package database with the docker
-apt-cache policy docker-ce
+apt-cache policy docker-ce \
 # Install Docker
-sudo apt install docker-ce
+sudo apt install docker-ce \
 # Update status docker
 sudo systemctl status docker
